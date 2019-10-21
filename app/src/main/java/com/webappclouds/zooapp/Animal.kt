f9adc7fa7c9ab03a83d701dbe4935982fa829c4b
Dan Boehmke <Daniel@webappclouds.com>
2019-10-21 12:12:22 -0400

    Animal adapter with layout inflator

--- app/src/main/java/com/webappclouds/zooapp/Animal.kt
+++ app/src/main/java/com/webappclouds/zooapp/Animal.kt
@@ -4,11 +4,12 @@ class Animal {
     var name: String? = null
     var desc: String? = null
     var image: Int? = null
+    var isKiller: Boolean? = null
 
-    constructor(name: String, desc: String, image: Int) {
+    constructor(name: String, desc: String, image: Int, isKiller: Boolean) {
         this.name = name
         this.desc = desc
         this.image = image
-
+        this.isKiller = isKiller
     }
 }
